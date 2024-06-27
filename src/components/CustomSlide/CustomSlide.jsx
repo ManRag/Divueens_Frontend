79cdfe3b028d5f10e3a6cf95f61728b9524dfd71
BaoiamApp <162950998+BaoiamApp@users.noreply.github.com>
2024-06-27 15:14:19 +0530

    Update CustomSlide.jsx

--- src/components/CustomSlide/CustomSlide.jsx
+++ src/components/CustomSlide/CustomSlide.jsx
@@ -4,11 +4,11 @@ import logo from '../../assets/logo.svg'
 
 const CustomSlide = ({ background, image, text, buttonText, buttonLink }) => {
   return (
-    <div className="custom-slide" style={{ backgroundImage: `url(${background})` }}>
+    <div className="custom-slide" style={{ backgroundImage: `url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.in%2FAttraction_Review-g295424-d10687494-Reviews-IMG_Worlds_of_Adventure-Dubai_Emirate_of_Dubai.html&psig=AOvVaw0CB4OLLXCmwjXstOlcLqwu&ust=1719567718715000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjtmYy_-4YDFQAAAAAdAAAAABAE)` }}>
       <div className="slide-content">
         <div className="slide-text-content">
         <div className="logo-container">
-            <img src={logo} alt="logo" className="slide-logo" />
+            <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.in%2FAttraction_Review-g295424-d10687494-Reviews-IMG_Worlds_of_Adventure-Dubai_Emirate_of_Dubai.html&psig=AOvVaw0CB4OLLXCmwjXstOlcLqwu&ust=1719567718715000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjtmYy_-4YDFQAAAAAdAAAAABAE alt="logo" className="slide-logo" />
           </div>
         <div className="slide-text" dangerouslySetInnerHTML={{ __html: text }}></div>
           <a href={buttonLink} className="slide-button">{buttonText}</a>
