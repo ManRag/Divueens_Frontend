4ce0ea9069632eeacb36bf6806c1140ad81fff76
Avyakta000 <100035215+Avyakta000@users.noreply.github.com>
2024-06-29 01:02:58 +0530

    bestSeller & brands segment

--- src/components/BestSeller/BestSeller.jsx
+++ src/components/BestSeller/BestSeller.jsx
@@ -1,111 +1,238 @@
-import React, { useState } from 'react';
-import './BestSeller.css';
-import { imageUrls } from '../../assets/assets';
+import React, { useEffect, useState } from 'react';
+import styles from './BestSeller.module.css';
+import background_prod, { imageUrls } from '../../assets/assets';
+import { GrPrevious, GrNext } from 'react-icons/gr';
+console.log(background_prod,'background')
+
+const trending = [
+    ...imageUrls
+].slice(0,8)
+const newArrivals = [
+    ...imageUrls
+].slice(8,16)
 
 const BestSeller = () => {
+    const [selectedCategory, setSelectedCategory] = useState('trending');
+    // slider
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [offset, setOffset] = useState(0)
 
     const [currentIndex, setCurrentIndex] = useState(0);
-    const imagesToShow = 4; // Number of images to show at a time
     const [activeIndex, setActiveIndex] = useState(0);
-    // Calculate the total number of slides
-    const totalSlides = Math.ceil(imageUrls.length / imagesToShow);
 
-    // Hover Buttons
-    const [isHovered, setIsHovered] = useState(false);
+    // buttons
+    const [isClicked, setIsClicked] = useState(false);
+    const [isClicked2, setIsClicked2] = useState(false);
 
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-       
-    };
+    // Hover effect
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const [hoveredImageIndex, setHoveredImageIndex] = useState(null);
 
-    const handleMouseLeave = () => {
-        setIsHovered(false);
 
-    };
+    //    slider begin
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+            console.log(screenWidth, 'screen width')
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [screenWidth]);
+
     
-    // hover buttons
+    // const totalImages = imageUrls.length;
+    const imagesToShow = Math.floor((screenWidth - 240) / 210); //extra(140,100,10) -100 for margin each image takes approximately 200px, extra 10px was added to ensure we get to hte end of the image other we'll get halfImage and a gap of 140px is reduced from the screen width
+
+    // console.log(maxOffset, 'maxoffset', imagesToShow)
+    // const maxOffset = 100 * (totalImages - imagesToShow);
+    const totalImages=[trending.length, newArrivals.length]
+
 
     const nextSlide = () => {
-        setCurrentIndex((prevIndex) =>
-            // num=prevIndex
-            prevIndex + imagesToShow >= imageUrls.length ? 0 : prevIndex + imagesToShow
+        if(selectedCategory==="trending"){
+            const maxOffset = 100 * (totalImages[0] - imagesToShow);
 
-        );
-        setActiveIndex((prevIndex) => (prevIndex === 1 ? 0 : prevIndex + 1));
+            setOffset(prevOffset => {
+                const newOffset = prevOffset + 100;
+                return newOffset > maxOffset ? 0 : newOffset;
+            });
+            console.log(maxOffset, imagesToShow, ' state;', offset)
 
+        }else{
+            const maxOffset = 100 * (totalImages[1] - imagesToShow);
+            setOffset(prevOffset => {
+                const newOffset = prevOffset + 100;
+                return newOffset > maxOffset ? 0 : newOffset;
+            });
+            console.log(maxOffset, imagesToShow, ' state;', offset)
+
+
+        }
     };
 
     const prevSlide = () => {
-        setCurrentIndex((prevIndex) =>
-            // console.log(prevIndex,'prevslide')
-            prevIndex === 0 ? imageUrls.length - imagesToShow : prevIndex - imagesToShow
+        if(selectedCategory==="trending"){
+            const maxOffset = 100 * (totalImages[0] - imagesToShow);
+            setOffset(prevOffset => {
+                const newOffset = prevOffset - 100;
+                return newOffset < 0 ? 0 : newOffset;
+            });
+        }else{
+            const maxOffset = 100 * (totalImages[1] - imagesToShow);
+            setOffset(prevOffset => {
+                const newOffset = prevOffset - 100;
+                return newOffset < 0 ? 0 : newOffset;
+            });
+        };
+    }
 
-        );
-        setActiveIndex((prevIndex) => (prevIndex === 0 ? 1 : prevIndex - 1));
-    };
+    // slider end
+
+    // top buttons
+    const HandleBtnClicked = (cat) => {
+        setSelectedCategory(cat)
+
+        // console.log(e.target)
+        setIsClicked2(false)
+        setIsClicked(true)
+    }
+    const HandleBtnClicked2 = (cat) => {
+        setSelectedCategory(cat)
+        setIsClicked(false)
+        // console.log(e.target)
+        setIsClicked2(true)
+    }
+    // top buttons end
 
 
-    const goToSlide = (index) => {
-        setCurrentIndex(index * imagesToShow);
-        setActiveIndex(index);
-    };
+    // hover functionality
+    const hoverStartHandler = (index) => {
+        console.log("hoverstart", index, styles.hover)
+        setHoveredIndex(index)
+    }
+    const hoverEndHandler = () => {
+        console.log("hoverend")
+        setHoveredIndex(null)
+    }
 
-    const currentImages = imageUrls.slice(currentIndex, currentIndex + imagesToShow);
+    const scaleImagehandler = (index) => {
+
+        setHoveredImageIndex(index)
+
+    }
+    const endScaleImagehandler = (index) => {
+
+        setHoveredImageIndex(null)
+
+
+
+    }
+
+    // select category logic
+    const renderProducts = () => {
+        if (selectedCategory === "trending") {
+
+            return (trending.map((image, index) => (
+
+                <div onMouseLeave={hoverEndHandler} onMouseEnter={() => { hoverStartHandler(index) }} key={index}
+                    style={{
+                        transform: `translateX(-${offset}%) `,
+                    }}
+                    className={`${styles["product-card"]}   ${hoveredIndex === index ? styles.hover : ''}`}>
+                    <div className={styles["image-wrapper"]} style={{ overflow: "hidden" }}>
+                        <img
+                            // onMouseLeave={hoverEndHandler} onMouseEnter={()=>{hoverStartHandler(index)}}
+                            onMouseEnter={() => { scaleImagehandler(index) }}
+                            onMouseLeave={endScaleImagehandler}
+                            style={{
+                                transform: hoveredImageIndex === index ? "scale(1.2)" : "scale(1)",
+                                transition: "transform 0.3s ease"
+                            }} src={image} alt={`product ${index + currentIndex}`}
+                        />
+                    </div>
+                    <div className={styles["product-info"]}>
+                        <h3>MAC' Face Mask</h3>
+                        <p>Rs. 5000/-</p>
+                        <button className={styles["buy-now"]}>Buy Now</button>
+                    </div>
+                </div>
+            )))
+
+        }
+        else if (selectedCategory === "newArrivals") {
+
+            return (newArrivals.map((image, index) => (
+
+                <div onMouseLeave={hoverEndHandler} onMouseEnter={() => { hoverStartHandler(index) }} key={index}
+                    style={{
+                        transform: `translateX(-${offset}%) `,
+                    }}
+                    className={`${styles["product-card"]}   ${hoveredIndex === index ? styles.hover : ''}`}>
+                    <div className={styles["image-wrapper"]} style={{ overflow: "hidden" }}>
+                        <img
+                            // onMouseLeave={hoverEndHandler} onMouseEnter={()=>{hoverStartHandler(index)}}
+                            onMouseEnter={() => { scaleImagehandler(index) }}
+                            onMouseLeave={endScaleImagehandler}
+                            style={{
+                                transform: hoveredImageIndex === index ? "scale(1.2)" : "scale(1)",
+                                transition: "transform 0.3s ease"
+                            }} src={image} alt={`product ${index + currentIndex}`}
+                        />
+                    </div>
+                    <div className={styles["product-info"]}>
+                        <h3>MAC' Face Mask</h3>
+                        <p>Rs. 5000/-</p>
+                        <button className={styles["buy-now"]}>Buy Now</button>
+                    </div>
+                </div>
+            )))
+
+        }
+    }
 
     return (
         <>
 
-            <div className="heading">
-                <hr className="hr-left" />
+            <div className={styles["heading"]}>
+                <hr className={styles["hr-left"]} />
                 <h2>Best Seller</h2>
-                <hr className="hr-right" />
+                <hr className={styles["hr-right"]} />
             </div>
-            <div className="subheading">
+            <div className={styles["subheading"]}>
                 <p>Your Cosmetics and Skincare Products</p>
             </div>
-            <div className="slideBtn bestseller_container">
-                <button onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}  style={{
-                        backgroundColor: isHovered ? '#ffff' : '#E9ACB6',
-                        color: isHovered ? '#000' : '#fff' ,
-                        transition: 'all 0.3s ease'
-                    }} class="first">New Arrivals</button>
-                <button onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave} style={{
-                        backgroundColor: isHovered ? '#E9ACB6':'#ffff' ,
-                        color: isHovered ? '#fff':'#000' ,
-                        transition: 'all 0.3s ease'
-                    }} className="second">Trending Now</button>
+            <div className={`${styles["slideBtn"]} ${styles["bestseller_container"]}`}>
+                <button onClick={() => { HandleBtnClicked("newArrivals") }}
+                    style={{
+                        backgroundColor: isClicked ? '#E9ACB6' : '#ffff',
+                        color: isClicked ? '#fff' : '#000',
+                    }} className={styles["first"]}>New Arrivals</button>
+                <button onClick={() => { HandleBtnClicked2("trending") }}
+                    style={{
+                        backgroundColor: isClicked ? '#ffff' : '#E9ACB6',
+                        color: isClicked ? '#000' : '#fff',
+                    }} className={styles["second"]}>Trending Now</button>
             </div>
-            <div className="product-container">
+            <div 
+            className={styles['products']}>
 
-                {currentImages.map((image, index) => (
-                    <div className="product-card ">
-                        <img src={image} alt={`product ${index + currentIndex}`} />
-                        <div className="product-info">
-                            <h3>MAC' Face Mask</h3>
-                            <p>Rs. 5000/-</p>
-                            <button>Buy Now</button>
-                        </div>
-                    </div>
-                ))}
+                <div className={styles["product-container"]} >
 
+                   {renderProducts()}
+
+                </div>
+                <div className={styles['controllers']}>
+                    <button onClick={prevSlide}><GrPrevious /></button>
+                    <button onClick={nextSlide}><GrNext /></button>
+
+                </div>
             </div>
-
-            <div class="pagination">
-
-                {Array.from({ length: totalSlides }).map((_, index) => (
-                    <button key={index} onClick={() => goToSlide(index)}>
-                        <span className={`dot ${activeIndex === index ? 'active' : ''}`}></span>
-                    </button>
-                ))}
-
-
-
-            </div>
-            <div class="see-all">
+            {/* <div className={styles["see-all"]}>
                 <button>See All</button>
-            </div>
+            </div> */}
 
         </>
 
