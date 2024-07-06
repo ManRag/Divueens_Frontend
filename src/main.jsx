edeeb119f70d2ac3564d7d4ab6b7fce2373025cd
Aayush-1205 <shindeaayush250@gmail.com>
2024-07-06 22:33:20 +0530

    minor error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-// import 'jquery'
-ReactDOM.createRoot(document.getElementById('root')).render(
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -14,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
->>>>>>> e5409af4fcb7b445239deb6cd27c1c8e61126b58
+
   <React.StrictMode>
     <BrowserRouter>
       <App />
