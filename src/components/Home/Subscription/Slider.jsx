80c8b2021a29f9bf19c03063d35dc7c074dc585e
ManmohanSinghRaghav <manmohan0singh0@gmail.com>
2024-07-07 20:45:09 +0530

    merge

--- src/components/Home/Subscription/Slider.jsx
+++ src/components/Home/Subscription/Slider.jsx
@@ -127,7 +127,7 @@ const slidesData = [
 
 const Item = ({ list }) => {
   return (
-    <div className="w-[17.5rem] h-[24.5em] bg-[#fff] overflow-hidden rounded-[10px] shadow-[0_4px_8px_#bbb] text-center transition-[0.3s_ease]">
+    <div className="w-[17.5em] h-[24.5em] bg-[#fff] overflow-hidden rounded-[10px] shadow-[0_4px_8px_#bbb] text-center transition-[0.3s_ease]">
       <img
         src={list.img}
         className="w-full h-auto rounded-t-[10px] object-cover"
@@ -152,12 +152,12 @@ const Item = ({ list }) => {
               <FaRegStar key={i} />
             ))}
           </div>
-          <div className="text-black my-[5px] mx-0 text-[12px] md:text-[18px] font-medium text-left">
+          <div className="text-black my-[5px] mx-0 text-[18px] font-medium text-left">
             {list.name}
-            <h3 className="text-[12px] md:text-[16px] text-[#333] font-bold">
+            <h3 className="text-[12px] text-[#333] font-bold">
               Rs. {list.price}/- <br />
             </h3>
-            <span className="text-[12px] md:text-[14px] text-[grey] font-[300]">
+            <span className="text-[12px] text-[grey] font-[300]">
               <del>Rs. {list.original_price}/-</del>
             </span>
             <span className="font-[300] text-[12px] text-[green]">
@@ -166,7 +166,7 @@ const Item = ({ list }) => {
           </div>
         </div>
         <Link to="/">
-          <button className="bg-rose-700 text-white border-[3px] border-rose-700 text-[12px] md:text-[15px] font-bold rounded-[8px] py-[5px] px-[10px] cursor-pointer shadow-[0px_1px_2px_#000] mb-[5px] hover:bg-white hover:text-rose-700">
+          <button className="text-sm bg-rose-700 text-white border-[3px] border-rose-700 text-[12px] font-bold rounded-[8px] py-[5px] px-[10px] sm:py-[2px] sm:px-[10px] cursor-pointer shadow-[0px_1px_2px_#000] mb-[5px] hover:bg-white hover:text-rose-700">
             Buy Now
           </button>
         </Link>
@@ -179,9 +179,25 @@ const Slider = () => {
   return (
     <Swiper
       navigation={true}
-      slidesPerView={4.3}
+      slidesPerView={1.2}
+      breakpoints={{
+        500: {
+          slidesPerView: 1.5,
+        },
+        650: {
+          slidesPerView: 2.2,
+        },
+        970: {
+          slidesPerView: 3.2,
+          // spaceBetween: 50,
+        },
+        1200: {
+          slidesPerView: 4.2,
+          // spaceBetween: 50,
+        },
+      }}
       modules={[Navigation]}
-      className="mySwiper w-[95vw] p-10"
+      className="mySwiper"
     >
       {slidesData.map((data, i) => (
         <SwiperSlide className="box-border p-10" key={i}>
