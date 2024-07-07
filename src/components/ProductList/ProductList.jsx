8013309fd6a0bc186df42cf3106b9e419fb3dfcf
Aayush-1205 <shindeaayush250@gmail.com>
2024-07-07 15:23:47 +0530

    layout - product list

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const ProductList = () => {
+    return (
+        <>
+            <div>
+                {/* Home > Skin */}
+                <div>
+
+                </div>
+
+                {/* Filters and Cards */}
+                <div>
+                    {/* Filters */}
+                    <div>
+
+                    </div>
+
+                    {/* Cards */}
+                    <div>
+                        {/* Makeup */}
+                        <div>
+
+                        </div>
+
+                        {/* Cards*/}
+                        <div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProductList
